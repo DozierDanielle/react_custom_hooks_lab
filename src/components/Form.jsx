782dee0a11784b167e3a51a06e966620f68d79c5
Dozier Danielle <dozierdanielle@ymail.com>
2025-09-20 07:41:40 +0300

    Complete React Custom Hooks Lab with useLocalStorage

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,23 +1,22 @@
 import React from 'react'
-import { useLocalStorage } from '../hooks/useLocalStorage'
-
+import useLocalStorage from '../hooks/useLocalStorage'
 export default function Form() {
   const [name, setName] = useLocalStorage('name', '')
   const [serviceNumber, setServiceNumber] = useLocalStorage('serviceNumber', '')
-
-  const handleSubmit = (e) => { e.preventDefault() }
-
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
+    <div style={{padding:'2rem', fontFamily:'sans-serif'}}>
+      <h2>Repair Form</h2>
+      <form>
+        <label>Name:</label>
         <input value={name} onChange={e => setName(e.target.value)} />
-      </label>
-      <label>
-        Service Number:
+        <br /><br />
+        <label>Service Number:</label>
         <input value={serviceNumber} onChange={e => setServiceNumber(e.target.value)} />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
+      </form>
+      <div style={{marginTop:'1rem'}}>
+        <strong>Current Name:</strong> {name} <br />
+        <strong>Current Service Number:</strong> {serviceNumber}
+      </div>
+    </div>
   )
 }
